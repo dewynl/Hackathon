ab98800d2cae44a28ca4d0fbc4a0db2ee379e4a0
Dewyn <dewyn.liriano@gmail.com>
2018-04-13 12:44:08 -0400

    Arreglo de identación en creador de los reportes.

--- reporte_puntaje/reportes_hackathon.py
+++ reporte_puntaje/reportes_hackathon.py
@@ -3,4 +3,4 @@ import pdfkit
 if __name__ == '__main__':
     for i in range(22):
         pdfkit.from_url('http://localhost:8000/detalle-puntos-reporte/' + str(i+1), 'reporte-equipo' + str(i+1) + '.pdf', )
-print('listo')
+    print('listo')
